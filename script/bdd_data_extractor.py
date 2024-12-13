76989bc436ce10849bd89e716c18943d51aa50b1
IlfGauhnith <lucasbburle@gmail.com>
2024-12-13 14:41:39 -0300

    treating

--- script/bdd_data_extractor.py
+++ script/bdd_data_extractor.py
@@ -1,5 +1,6 @@
 import basedosdados as bd
 import sys
+import os
 
 # script name counts as argument. 
 EXPECTED_SYS_ARG_COUNT = 4
@@ -14,7 +15,14 @@ if __name__ == '__main__':
     billing_project_id = sys.argv[2]
     output_file = sys.argv[3]
 
+    if os.path.splitext(output_file)[1] != '.csv':
+        raise ValueError("Invalid extension for output file. Expected .csv extension.")
+    
+    if os.path.exists(output_file):
+        print(f"Data already downloaded. Available at {os.path.abspath(output_file)}")
+        exit()
+
     query = open(query_file).read()
 
     df = bd.read_sql(query = query, billing_project_id = billing_project_id)
-    df.to_csv(output_file)
+    df.to_csv(output_file, index=False)
